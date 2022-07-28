eb10bcbe23a1afbe8cfd63f517a4816d021ecd62
Kanishk <rawat27kanishk@gmail.com>
2022-07-29 01:23:57 +0530

    Loops : For loop

---
+++ CPP(Basics)/loop/forloop/forloop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    int n ;
+    cin>>n;
+    for(int i=1;i<n;i++){
+        cout << i <<endl;
+    }
+}
