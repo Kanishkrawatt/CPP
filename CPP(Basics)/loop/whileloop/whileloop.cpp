39904074f7213b58244435ffa41d67c47ba5a0a4
Kanishk <rawat27kanishk@gmail.com>
2022-07-29 01:24:42 +0530

    Loop :while Loop

---
+++ CPP(Basics)/loop/whileloop/whileloop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    int n;
+    cin >>n;
+    while(n>0){
+        cout<<n<<endl;
+        n--;
+    }
+}
