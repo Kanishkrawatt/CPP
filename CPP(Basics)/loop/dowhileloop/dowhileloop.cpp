de8daa31a7cdf1770ad78d60dd568ca84f0cc8d9
Kanishk <rawat27kanishk@gmail.com>
2022-07-29 01:28:56 +0530

    Loop : Do While Loop

---
+++ CPP(Basics)/loop/dowhileloop/dowhileloop.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    #ifndef FileSystem
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+
+    #endif
+    int n;
+    cin >> n;
+    do
+    {
+        cout << n<<"\n";
+        n--;
+    } while (n>0);
+    
+}
