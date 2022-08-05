4845c806c302e560314c8339afa56e09c856ceab
Kanishk <rawat27kanishk@gmail.com>
2022-08-05 23:36:12 +0530

    Mooc competitive Coding

---
+++ text.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    if((2^1) & 2==2){
+        cout<< "hi";
+    }
+    else{
+        cout << "bue";
+    }
+    
+    return 0;
+}
