cd4fde9c6fe19a48be7c3efaa122a863ea6ae9c2
Kanishk <rawat27kanishk@gmail.com>
2022-09-01 16:54:31 +0530

    Added Array Searching

--- CPP(DSA)/Array/SearchingInArray/BinarySearch.cpp
+++ CPP(DSA)/Array/SearchingInArray/BinarySearch.cpp
@@ -16,4 +16,23 @@ int main(){
     int key;
     cout<< "Enter The Number to check if it is in Array  : ";
     cin>> key;
+    int lb = 0;
+    int ub = n;
+    while(lb<=ub){
+        int mid = (lb+ub)/2;
+        if(arr[mid]==key){
+            cout<<"Number Found at "<<mid<<" index"<<endl;
+            break;
+        }
+        else if(arr[mid]>key){
+            ub=mid-1;
+        }
+        else{
+            lb = mid+1;
+        }
+    }
+    if(lb>ub){
+        cout<<"Number Not Found";
+    }
+
 }
--- CPP(DSA)/Array/SearchingInArray/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-int key;
-    cout<< "Enter The Number to check if it is in Array  : ";
-    cin>> key;
