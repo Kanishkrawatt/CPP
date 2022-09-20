86e45f3d106673b83f5631c806cbe0ca90afa6b8
Kanishk <rawat27kanishk@gmail.com>
2022-09-21 00:16:38 +0530

    Added More Solution in The assignment

---
+++ CPP(Basics)/College/Assignment1/Q4FindTheSimpleIntrest.cpp
@@ -0,0 +1,16 @@
+// A = P (1 + rt)
+// Find the Simple Intrest 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r,t,p;
+    cout<<"P : ";
+    cin>>p;
+    cout<<"R : ";
+    cin>>r;
+    cout<<"T : ";
+    cin>>t;
+    cout<<"Amount = "<<p*(1+r*t)<<endl;
+    return 1;
+}
---
+++ CPP(Basics)/College/Assignment1/Q5WapToDisplayAgeNameAddress.cpp
@@ -0,0 +1,22 @@
+// Wap to display name, age and address
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main(){
+    string name,address;
+    int age;
+    cout<<"Name : ";
+    getline(cin,name);
+    cout<<"Age : ";
+    cin>>age;
+    cout<<"Address : ";
+    cin.ignore();
+    getline(cin,address);
+    cout <<"Output"<<endl;
+    cout<<"Name : "<<name<<endl;
+    cout<<"Age : "<<age<<endl;
+    cout<<"Address : "<<address<<endl;
+    return 1;
+}
