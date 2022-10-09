27f1ab30bc8abf3b9d46e1dad3b333762950cf3b
Kanishk <rawat27kanishk@gmail.com>
2022-10-09 23:51:42 +0530

    Added Algorithms

---
+++ CPP(Basics)/Algorithms/SieveOfEratosthenes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void primeSieve(int n){
+    int prime[100] = {0};
+    for(int i=2;i<=n;i++){
+        if(prime[i]==0){
+            for(int j=i*i;j<=n;j++){
+                prime[i]=1;
+            }
+        }
+    }
+    for(int i=2;i<n;i++){
+        if(prime[i]==0){
+            cout<<i<<" ";
+        }
+    }cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    primeSieve(n);
+}
