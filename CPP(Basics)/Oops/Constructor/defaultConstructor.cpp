9373722e284566bf0211014635693bed244c2fc7
Kanishk <rawat27kanishk@gmail.com>
2022-11-18 12:05:40 +0530

    Added Constructors

---
+++ CPP(Basics)/Oops/Constructor/defaultConstructor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Default_Constructor{
+    public:
+        int num;
+        Default_Constructor(){
+            num = 10;
+        }
+};
+int main(){
+    Default_Constructor D1;
+    cout<<D1.num;
+}
